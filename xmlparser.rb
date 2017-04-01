3a3555f711cee9dc46926be68cc3ef3ca919c894
Josh Kremer <joshkremer@gmail.com>
2017-04-01 07:02:03 -0700

    added site name

--- xmlparser.rb
+++ xmlparser.rb
@@ -1,5 +1,7 @@
 require 'sitemap-parser'
 require 'csv'
+
+sitename = "New District"
 sitemap = SitemapParser.new "https://newdistrict.ca/sitemap.xml"
 sitemap.urls # => Array of Nokigiri XML::Node objects
 sitemap.to_a # => Array of url strings
@@ -9,7 +11,7 @@ link_array = sitemap.to_a
 puts link_array
 
 link_array.each do |i|
-	CSV.open("file.csv", "a+") do |csv|
+	CSV.open("#{sitename}.csv", "a+") do |csv|
 	  csv << [i]
   end
 end
